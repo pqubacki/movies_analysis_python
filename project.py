f7f8373fb67e046518dd1c09d1d6490a3073a505
Piotr Kubacki <pqubacki@gmail.com>
2021-12-22 02:09:44 +0000

    Adds basic functionality and the skeleton for the project

--- project.py
+++ project.py
@@ -4,16 +4,21 @@ Created on Tue Dec  7 10:10:48 2021
 
 @author: Piotr Kubacki
 """
+
+import sys
 import pandas as pd
 
 
 
 #definition of function displaying initial menu for the user
 def menu():
+
     """Displays main menu with 6 options to choose from."""
+
     print(
     """
     Please select one of the following options:
+
     1. Most successful directors or actors 
     2. Film comparison 
     3. Analyse the distribution of gross earnings 
@@ -23,56 +28,75 @@ def menu():
     """
     )
         
-    try:
-        
-        #defines integer variable carrying user's choice    
-        option = int(input("Enter numeric option to process dataset: "))
-        
-        if option == 1:
-            print( "1. Most successful directors or actors")
-            successfulDirectorsActors()
-        elif option == 2:
-            print("2. Film comparison")
-            filmComparison()
-        elif option == 3:
-            print("3. Analyse the distribution of gross earnings")
-            grossEarnDistibution()
-        elif option == 4:
-            print("4. Self-Directing")
-            selfDirect()
-        elif option == 5:
-            print("5. Earnings and IMDB scores")
-            earningsAndScores()
-        elif option == 6:
-            print("Exiting...")
+    while True:
+
+        try:
+
+            option = int(input("Enter numeric option to process dataset: "))
+
+        except ValueError:
+
+            print("Option must be valid integer.")
+
         else:
-            print("Make sure you enterd number of valid option from 1 to 6.")
-        
-    except ValueError:
-        print("Option must be valid integer.")
+
+            if  option == 1:
+
+                successfulDirectorsActors()
+
+            elif option == 2:
+
+                print("2. Film comparison")
+                filmComparison()
+
+            elif option == 3:
+
+                print("3. Analyse the distribution of gross earnings")
+                grossEarnDistibution()
+
+            elif option == 4:
+
+                print("4. Self-Directing")
+                selfDirect()
+                
+            elif option == 5:
+
+                print("5. Earnings and IMDB scores")
+                earningsAndScores()
+
+            elif option == 6:
+
+                print("Exiting...")
+                sys.exit()
+
+            #for inputs not generating exception but beyond the range 1-6
+            else:
+                
+                print("Make sure you entered number from 1 to 6 representing valid option.")
+
         
 def successfulDirectorsActors():
-    pass
+    print( "1. Most successful directors or actors")
+    print(df.head())
+    
 
 def filmComparison():
     pass
 
 def grossEarnDistibution():
     pass
+
 def selfDirect():
     pass
 
 def earningsAndScores():
     pass
 
-def meniu(**options):
-    print(options)
-    
-
-
 def main():
-    #df = pd.read_csv("movie_metadata.csv")
-    #print(df)
+    
+    
+    df = pd.read_csv("movie_metadata.csv")
+
     menu()
 
 main()
